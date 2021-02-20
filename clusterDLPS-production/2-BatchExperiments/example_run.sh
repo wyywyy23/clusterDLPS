025024b5937c9d64274585fcf8d1ab2e8ecc5576
Yuyang Wang <yuyang_wang@umail.ucsb.edu>
2021-02-19 21:59:10 -0800

    example_run

--- clusterDLPS-production/2-BatchExperiments/example_run.sh
+++ clusterDLPS-production/2-BatchExperiments/example_run.sh
@@ -3,5 +3,5 @@
 for mode in none on-off laser full
 
 do
-./wyy_simulator --activate-dlps --log=wyy_simulator.th:info --log=wyy_wms.th:info --log=wyy_wms.fmt:%m --log=wyy_wms.app:file:output/task_execution_${mode}.csv --log=dlps.th:info --log=dlps.fmt:%m --log=dlps.app:file:output/dlps_events_${mode}.csv --cfg=contexts/guard-size:0 platforms/cluster_64_machines_FAT_TREE.xml ../instance_trace_to_workflows/original/output/swf/container_trace.swf ../instance_trace_to_workflows/spar_sampled/workflows/64_machines/ 4096 static --cfg=network/dlps:${mode}
+./wyy_simulator --activate-dlps --cfg=network/crosstraffic:0 --log=wyy_simulator.th:info --log=wyy_wms.th:info --log=wyy_wms.fmt:%m --log=wyy_wms.app:file:output/task_execution_${mode}.csv --log=dlps.th:info --log=dlps.fmt:%m --log=dlps.app:file:output/dlps_events_${mode}.csv --cfg=contexts/guard-size:0 platforms/cluster_4_machines_FAT_TREE.xml ../instance_trace_to_workflows/original/output/swf/container_trace.swf ../instance_trace_to_workflows/spar_sampled/workflows/4_machines/ 4096 static --cfg=network/dlps:${mode}
 done
