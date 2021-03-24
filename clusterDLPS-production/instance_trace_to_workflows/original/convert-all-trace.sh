487c0ee905c5904ae4f8df5d89129507c179c15b
Yuyang Wang <yuyang_wang@umail.ucsb.edu>
2021-03-23 22:17:26 -0700

    generate more dags

--- clusterDLPS-production/instance_trace_to_workflows/original/convert-all-trace.sh
+++ clusterDLPS-production/instance_trace_to_workflows/original/convert-all-trace.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 START_OFFSET=48
-DURATION=24
+DURATION=1
 
-WORKFLOW_DIR=./output/workflows
+WORKFLOW_DIR=./output/workflows_more_dags
 END=`expr ${DURATION} - 1`
 
 for (( START_HOUR=0; START_HOUR<=${END}; START_HOUR++ ))
@@ -16,6 +16,6 @@ do
 #    done
 done
 
-./trace2workflows ${START_OFFSET} ${DURATION} 0.015 1000
+./trace2workflows ${START_OFFSET} ${DURATION} 1 1000
 
 ./trace2swf
